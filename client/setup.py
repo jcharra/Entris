a22f853e7a6f205b06a8219c0bcf117c5f93dfac
jcharra <jcharra@07d75a7c-3e0e-46f4-a616-739fe066dda5>
2010-10-23 20:59:59 +0000

    redundant
    git-svn-id: svn+ssh://svn.code.sf.net/p/entris/code@37 07d75a7c-3e0e-46f4-a616-739fe066dda5

--- client/setup.py
+++
@@ -1,7 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-        name = "hello",
-        version = "0.1",
-        description = "the typical 'Hello, world!' script",
-        executables = [Executable("entris.py")])
